db7a75b01c281f3d4e6d774e785b97dfc98e638a
Sara4morim <saraamorimjesus10@gmail.com>
2024-12-03 09:53:58 -0300

    Add files via upload

---
+++ aula02.py
@@ -0,0 +1,12 @@
+print ('bom dia, como vai?')
+print ('o dia mais triste da minha semana, é terça')
+nome_usuario = 'sara amorim' #
+idade_usuario= 18 
+altura_usuario= 1.61
+maior_idade= True
+
+print('Seja bem vinda', nome_usuario, 'sua idade é idade_usuario anos, você tem altura_usuario metros. Você é maior de idade? maior idade')
+print ('seja bem vinda', nome_usuario,)
+print('sua idade é', idade_usuario, 'anos')
+print ('você tem', altura_usuario, 'metros')
+print ('você é maior de idade?', maior_idade)
